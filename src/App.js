0706a150ceb2bd6337a02100f45f51b56ec75e8d
yashwanthg7 <yashwanth18183@gmail.com>
2023-02-24 13:41:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { styled } from '@mui/material';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+// import ProtectedRoute from './components/ProtectedRoute';
+// import PublicRoute from './components/PublicRoute';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+import CoinPage from './Pages/CoinPage';
+import Homepage from './Pages/Homepage';
+import LoginPage from './Pages/LoginPage';
+import Register from './Pages/Register';
+
+function App() {
+
+
+
+  //styling starts here
+
+  const CustomApp = styled('div')(({ theme }) => ({
+    backgroundColor: "#031B34",
+    color: "white",
+    minHeight: "100vh",
+  }))
+
+  //styling ends here
+
+
+
+
+  return (
+    <BrowserRouter>
+      <CustomApp>
+        <Header />
+
+        {/* your private and public routes goes here */}
+        <PrivateRoute path='/login' component={LoginPage} />
+        <PrivateRoute path='/register' component={Register} />
+        <PrivateRoute path="/home" component={Homepage}/>
+        <PrivateRoute path='/coins/:id' component={CoinPage} />
+        
+      </CustomApp>
+    </BrowserRouter>
+  );
+}
+
+export default App;
